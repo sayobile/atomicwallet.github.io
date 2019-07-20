582d20f9acc889d731898d7841ab33542977c00e
Emilio1111 <lyricstyle@bk.ru>
2019-07-21 01:09:23 +0500

    Modified Request api

--- test/js/search.js
+++ test/js/search.js
@@ -1,7 +1,8 @@
 $(function(){
 	var items,
 	 resultBlock = document.getElementById('body-table-data'),
-	 prices;
+	 prices,
+	 additionals;
 	 
 	 async function getData(){
 		 
@@ -9,10 +10,12 @@ $(function(){
 
 
 		 const stringTicket = 'BTC,XRP,ETH,BCH,XLM,EOS,LTC,USDT,ADA,XMR,BSV,TRX,DASH,BNB,ETC,NEO,ONT,XTZ,ZEC,XVG,WAVES,BTG,VET,LSK,MKR,OMG,MGO,DOGE,ZRX,DCR,QTUM,AE,ZIL,BCD,DGB,NPXS,PMA,MITH,TUSD,GUSD,AWC,USDC,PPT,PAX,REP,GNT,HOT,IOST,WAX,BZNT,DGTX,MANA,WTC,NEXO,BNT,TPAY,BTT,DAI,VTHO,SMART,EURS,ABYSS,MXM,XYO,ODE,1ST,300,ADST,ADT,ADX,AIR,ALIS,AMIS,ANT,APT,ARC,ARN,ART,ATH,IOTX,ATL,AVT,BCDN,BET,BLX,BMC,BMT,BNC,BRAT,BQX,BTE,CAG,CAT,CCC,CDT,CFI,CMC,COB,CRB,CREDO,CVC,DATA,DDF,DENT,DICE,DNT,DRP,EDG,ELIX,EURT,FRD,FUEL,FUN,GBT,GNO,GUP,HGT,HST,ICE,ICN,ICOS,IFT,IND,IXT,JET,KICK,KIN,KNC,RFR,LATX,LIFE,LINK,LNC,LRC,LUN,MCAP,MCO,MDA,MNE,MSP,MTH,MTX,MYST,NET,NMR,NXX,OAX,OPT,PAY,PIX,PLAY,PLBT,PLR,PLU,POE,POS,PRO,PTOY,QAU,BAT,RLT,RLX,RVT,SALT,SCL,SENSE,SKIN,SNC,SNGLS,SND,SNM,SNT,STX,SUB,SWT,TaaS,TBT,TFL,TIME,TIX,TKN,TNT,TRST,VERI,VIBE,VIB,VIU,VRS,VSL,WiC,WINGS,WOLK,XAUR,XID,XNN,XRL,BCPT,BIX,R,GTO,SRN,OCN,ELF,GVT,EVX,TNB,RUFF,AMB,THETA,POLY,APPC,JNT,QUN,NAS,DTA,SXUT,LEND,POWR,ITC,RCN,ENJ,RDN,MTN,REQ,WPR,DLT,GNX,AST,CMT,AIDOC,YOYOW,NULS';	
-		 const firstResponse = await fetch(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${stringTicket}&tsyms=USD`);
+		 const firstResponse = await fetch("https://min-api.cryptocompare.com/data/pricemultifull?fsyms="+stringTicket+"&tsyms=USD");
 		 const dataInfo = await firstResponse.json();
 		 const secondResponse = await fetch("../assets.js");
 		 const dataNames = await secondResponse.json();
+		 const thirdResponse = await fetch("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=MOD,UKG,BRD,GTC,BKX,MDS,CND,ENG,DPY,C20,LEV,ATM,STORM,MOF,QSP,QASH,CS,DRGN,ETHOS,DCN,NOW&tsyms=USD");
+		 const thirdInfo = await thirdResponse.json();
 		 
 		 
 		
@@ -21,22 +24,9 @@ $(function(){
 		 
 		 prices = dataInfo.DISPLAY;
 		 items = dataNames;
-		 console.log(items.length -1);
+		 additionals = thirdInfo.DISPLAY;
 
 
-		var tickets = new Array();
-		 $.each(items,function(k,v){
-			
-			tickets.push(v.name);
-		 })
-
-	
-
-		 var string = tickets.join(',');
-
-		
-	
-		 console.log(prices);
 		 $.each(prices,function(key,val){
 			 $.each(items,function(k,item){
 				 if(key == item.name){
@@ -45,12 +35,24 @@ $(function(){
 					 item.cap = val.USD.MKTCAP;
 					 item.change = val.USD.CHANGEPCT24HOUR; 
 				 }
-				 
-				 
 			 })
 			 
 		 })
 		 
+		 $.each(additionals,function(ke_y,value){
+			$.each(items,function(k,i){
+				if(ke_y == i.name){
+					console.log(ke_y);
+					i.price = value.USD.PRICE;
+					i.volume = value.USD.TOTALVOLUME24HTO;
+					i.cap = value.USD.MKTCAP;
+					i.change = value.USD.CHANGEPCT24HOUR; 
+				}
+				
+				
+			})
+			
+		})
 	
 		
 		 renderList(items,resultBlock);
